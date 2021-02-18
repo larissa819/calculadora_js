9451c3afd60378f34b9e1c9c8ecd0c8824c8793f
larissa819 <76538084+larissa819@users.noreply.github.com>
2021-02-17 21:21:16 -0300

    js

---
+++ js/calculadora.js
@@ -0,0 +1,10 @@
+var n1= prompt("Digite o primeiro numero:");
+var n2= prompt("Digite o segundo numero:");
+
+var int1 = parseInt(n1);
+var int2 = parseInt(n2);
+var soma = int1 + int2;
+
+console.log(soma);
+
+alert("Resultado=" + soma);
